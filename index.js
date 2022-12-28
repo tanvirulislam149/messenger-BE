627adde8a9cc018fdee4b836b584278eb0c07afd
tanvir <tanvirulislam149@gmail.com>
2022-12-28 23:51:36 +0600

    setup done

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require('express')
+const express = require('express');
+const chats = require('./data');
+const cors = require("cors");
+require("dotenv").config();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// middleware
+app.use(cors());
+
+app.get('/api/v1/chats', (req, res) => {
+  res.send(chats);
+})
+
+app.get("/api/v1/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 })
 
 app.listen(port, () => {
